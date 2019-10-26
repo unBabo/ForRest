b37598c33f24e070d61730d9d381041e60dba66a
unBabo <42173201+unBabo@users.noreply.github.com>
2019-10-27 06:25:00 +0900

    画像送受信まで

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 const io = require('socket.io')(http);
+// var fs =require('fs');
 const PORT = process.env.PORT || 7000;
+app.use(express.static(__dirname));
+// var mime = {
+//   ".html": "text/html",
+//   ".css":  "text/css"
+//   // 読み取りたいMIMEタイプはここに追記
+// };
 
 var parser = require('ua-parser-js');
 // var parser = new UAParser();
@@ -13,6 +20,7 @@ var parser = require('ua-parser-js');
 app.get('/' , function(req, res){
   // var ua = parser(req.headers['user-agent']);     // HTTPヘッダよりUser agentを取得
    res.sendFile(__dirname+'/public/index.html');
+   // res.writeHead(200, {"Content-Type": mime[path.extname(__dirname+'/public/index.html')] || "text/plain"});
   // console.log(ua.device);
   // if(ua.device.model!=undefined){
   //   res.sendFile(__dirname+'/public/index.html');
@@ -25,9 +33,9 @@ io.on('connection',function(socket){
     socket.on('message',function(msg){
         io.emit('message',msg);
     });
-    // socket.on('canvassend',function(canvasdata){
-    //   io.emit(canvassend,canvasdata);
-    // });
+    socket.on('canvassend',function(canvasdata){
+      io.emit('canvassend',canvasdata);
+    });
 });
 
 http.listen(PORT, function(){

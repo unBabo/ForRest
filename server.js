a77becf85a18cde48b2bec298bf46f885467cd26
unBabo <42173201+unBabo@users.noreply.github.com>
2019-10-27 03:22:18 +0900

    error

--- server.js
+++ server.js
@@ -11,20 +11,23 @@ var parser = require('ua-parser-js');
 //console.log(parser);
 
 app.get('/' , function(req, res){
-  var ua = parser(req.headers['user-agent']);     // HTTPヘッダよりUser agentを取得
+  // var ua = parser(req.headers['user-agent']);     // HTTPヘッダよりUser agentを取得
+   res.sendFile(__dirname+'/public/index.html');
   // console.log(ua.device);
-  if(ua.device.model!=undefined){
-    res.sendFile(__dirname+'/public/index.html');
-  }else{
-    res.sendFile(__dirname+'/p5Practice.html');
-  }
+  // if(ua.device.model!=undefined){
+  //   res.sendFile(__dirname+'/public/index.html');
+  // }else{
+  //   res.sendFile(__dirname+'/p5Practice.html');
+  // }
 });
 
 io.on('connection',function(socket){
     socket.on('message',function(msg){
-        // console.log('message: ' + msg);
         io.emit('message',msg);
     });
+    // socket.on('canvassend',function(canvasdata){
+    //   io.emit(canvassend,canvasdata);
+    // });
 });
 
 http.listen(PORT, function(){
